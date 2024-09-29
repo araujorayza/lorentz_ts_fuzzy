86f30509e72f85061c67bb83010a35ce3c07454d
araujorayza <47280806+araujorayza@users.noreply.github.com>
2024-09-29 15:38:12 -0300

    add time to improve the plot

--- compare_fuzzy_nonlinear.m
+++ compare_fuzzy_nonlinear.m
@@ -8,24 +8,21 @@ ulim=[-60,60];
 vlim=[-80,60];
 wlim=[-60+5/4*param(2),80+5/4*param(2)];
 % using the change of variables: x=u, v=y, z=w-5/4*r
-% u=1;
-% v=2;
-% w=3;
+
 
 InitSTATE=[20;-70;40+5/4*param(2)];
-t=0:0.001:50;
+t=0:0.001:30;
 
-options = odeset('RelTol',1e-14,'AbsTol',1e-14);
-tol=8;
+options = odeset('RelTol',1e-13,'AbsTol',1e-13);
 
-[~,STATE_NL] = ode45(@(t,y_var) lorentz_nonlinear(t,y_var,param),t,InitSTATE,options);
-[~,STATE_TS] = ode45(@(t,y_var) lorentz_TSFuzzy(t,y_var,param),t,InitSTATE,options);
+[t_nl,STATE_NL] = ode45(@(t,y_var) lorentz_nonlinear(t,y_var,param),t,InitSTATE,options);
+[t_ts,STATE_TS] = ode45(@(t,y_var) lorentz_TSFuzzy(t,y_var,param),t,InitSTATE,options);
 
 
 figure(1)
-plot(t,STATE_TS(:,1),'-k')
+plot(t_ts,STATE_TS(:,1),'-k')
 hold on
-plot(t,STATE_NL(:,1),'-r')
+plot(t_nl,STATE_NL(:,1),'-r')
 title('u x t')
 
 figure(2)
