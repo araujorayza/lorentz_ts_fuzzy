575226647f927ab0691b1a74dde5c88438131ca1
araujorayza <47280806+araujorayza@users.noreply.github.com>
2024-09-29 15:49:34 -0300

    use deval to obtain more accurate time values

--- compare_fuzzy_nonlinear.m
+++ compare_fuzzy_nonlinear.m
@@ -11,30 +11,35 @@ wlim=[-60+5/4*param(2),80+5/4*param(2)];
 
 
 InitSTATE=[20;-70;40+5/4*param(2)];
-t=0:0.001:30;
+t=[0 100];
 
 options = odeset('RelTol',1e-13,'AbsTol',1e-13);
 
-[t_nl,STATE_NL] = ode45(@(t,y_var) lorentz_nonlinear(t,y_var,param),t,InitSTATE,options);
-[t_ts,STATE_TS] = ode45(@(t,y_var) lorentz_TSFuzzy(t,y_var,param),t,InitSTATE,options);
+%resolve a edo no intervalo
+sol_nl = ode45(@(t,y_var) lorentz_nonlinear(t,y_var,param),t,InitSTATE,options);
+sol_ts = ode45(@(t,y_var) lorentz_TSFuzzy(t,y_var,param),t,InitSTATE,options);
 
+%calculo a solução nos pontos exatos que quero
+t=linspace(0,100,250);
+STATE_NL=deval(sol_nl,t);
+STATE_TS=deval(sol_ts,t);
 
 figure(1)
-plot(t_ts,STATE_TS(:,1),'-k')
+plot(t,STATE_TS(1,:),'-k')
 hold on
-plot(t_nl,STATE_NL(:,1),'-r')
+plot(t,STATE_NL(1,:),'-r')
 title('u x t')
 
 figure(2)
-plot(t,STATE_TS(:,2),'-k')
+plot(t,STATE_TS(2,:),'-k')
 hold on
-plot(t,STATE_NL(:,2),'-r')
+plot(t,STATE_NL(2,:),'-r')
 title('v x t')
 
 figure(3)
-plot(t,STATE_TS(:,3),'-k')
+plot(t,STATE_TS(3,:),'-k')
 hold on
-plot(t,STATE_NL(:,3),'-r')
+plot(t,STATE_NL(3,:),'-r')
 title('w x t')
 
 
