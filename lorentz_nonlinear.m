adcbbb2ebd52ee58845392f33afb63ebc4b76b2f
araujorayza <47280806+araujorayza@users.noreply.github.com>
2025-01-02 15:43:35 -0300

    plotting the new nonlinear model

--- lorentz_nonlinear.m
+++ lorentz_nonlinear.m
@@ -9,13 +9,23 @@ function dstate=lorentz_nonlinear(t,state,param)
     r = param(2);
     b = param(3);
 
-    u = state(1);
-    v = state(2);
-    w = state(3);
+%     u = state(1);
+%     v = state(2);
+%     w = state(3);
+% 
+%     du = -sigma*u + sigma*v;
+%     dv = -v -u*w + r*u;
+%     dw = -b*w + u*v;
+% 
+%     dstate = [du;dv;dw];             
 
-    du = -sigma*u + sigma*v;
-    dv = -v -u*w + r*u;
-    dw = -b*w + u*v;
+    x = state(1);
+    y = state(2);
+    z = state(3);
 
-    dstate = [du;dv;dw];             
+    dx = -sigma*x + sigma*y;
+    dy = -y -x*z + r*x;
+    dz = -b*z + x*y;
+
+    dstate = [dx;dy;dz];
 end
